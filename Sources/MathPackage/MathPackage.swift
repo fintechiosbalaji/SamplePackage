fb371195e8f67279cd5667a9ab43769fdb3a86e6
fintechiosbalaji <149308306+fintechiosbalaji@users.noreply.github.com>
2023-11-30 14:35:57 +0530

    Initial commit

--- Sources/MathPackage/MathPackage.swift
+++ Sources/MathPackage/MathPackage.swift
@@ -1,2 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+// MathUtils.swift
+public struct MathUtils {
+    public static func square(_ number: Int) -> Int {
+        return number * number
+    }
+}
